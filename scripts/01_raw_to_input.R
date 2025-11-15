16005739408578edf3f92ed286f96ffda0571441
miihcr <milena.costaramos@hotmail.com>
2025-11-15 17:38:50 +0100

    Updated 01_raw_to_input.R

--- scripts/01_raw_to_input.R
+++ scripts/01_raw_to_input.R
@@ -9,10 +9,6 @@ sales_raw <- read_csv("01_raw/data/sales.csv",
   relocate(sale_id)
              
 
-
-
-
-
 addresses_raw    <- read_csv("01_raw/data/addresses.csv")
 dwellings_raw    <- read_csv("01_raw/data/dwellings.csv")
 public_spaces_raw<- read_csv("01_raw/data/public_spaces.csv")
@@ -20,7 +16,7 @@ towns_raw        <- read_csv("01_raw/data/towns.csv")
 municipalities_raw <- read_csv("01_raw/data/municipalities.csv")
 
 
-
+str(sales_input)
 # 2. Clean SALES
 
 
@@ -52,6 +48,77 @@ sales_input <- sales_raw |>
     price_eur = sales_price * 1000,
     # convert sale_date to Date
     sale_date = ymd(sale_date)
-  )
+  ) |> 
+  select(sale_id, street_name, house_number, house_addition,
+         postcode, city, sale_date, price_eur,
+         public_spaces_id)
 
-str(sales_input)
+# 3. Clean ADDRESSES
+
+addresses_input <- addresses_raw |> 
+  rename(
+    house_number   = huisnummer,
+    house_addition = huisnummertoevoeging
+  ) |> 
+  mutate(
+    house_number     = as.integer(house_number),
+    house_addition   = house_addition |> 
+      trimws() |> 
+      str_to_upper() |> 
+      tidyr::replace_na(""),
+    address_id       = as.character(id),
+    openbareruimte   = as.character(openbareruimte),
+    start_valid      = begin_geldigheid,
+    end_valid        = eind_geldigheid,
+    public_space_id   = as.character(openbareruimte)
+  ) |> 
+  select(address_id, postcode, house_number, house_addition,
+         public_space_id)
+
+
+# 4. Clean PUBLIC SPACES
+
+str(public_spaces_raw)
+
+
+public_spaces_input <- public_spaces_raw |> 
+  mutate(
+    public_space_id   = as.character(id),
+    public_space_name = str_to_lower(naam),
+    public_space_type = str_to_lower(type),
+    town_id          = as.character(woonplaats),
+    start_valid      = begin_geldigheid,
+    end_valid        = eind_geldigheid
+  ) |> 
+  select(public_space_id, public_space_name, public_space_type,
+         town_id, start_valid, end_valid)
+
+str(public_spaces_input)
+
+str(dwellings_raw)
+
+# 5. Clean DWELLINGS
+
+dwellings_input <- dwellings_raw |> 
+  mutate(
+    dwelling_id = as.character(id),
+    address_id = as.character(hoofdadres),
+    id_building = as.character(pand),
+    start_valid      = begin_geldigheid,
+    end_valid        = eind_geldigheid
+  ) |> 
+  select(dwelling_id, address_id, id_building, start_valid,
+         end_valid, gebruiksdoel, oppervlakte, x, y)
+
+# 6. Clean TOWNS
+str(towns_raw)
+towns_input <- towns_raw |> 
+  mutate(
+    town_id = as.character(id),
+    town_name = str_to_lower(naam),
+    start_valid      = begin_geldigheid,
+    end_valid        = eind_geldigheid
+  ) |> 
+  select(town_id, town_name, start_valid, end_valid,
+         status, x, y)
+
