091bdc21eeedc9b2f57b28f9717fce5d158d6548
miihcr <milena.costaramos@hotmail.com>
2025-11-27 11:27:47 +0100

    Added output

--- scripts/output.R
+++ scripts/output.R
@@ -1,13 +1,15 @@
 # output (not complete)
 
-final_output <- read_rds("03_final/output/final_dataset.rds")
+final_output <- read_rds("data/processed/final_dataset.rds")
 
 avg_prices_denbosch_2024 <- final_output |> 
   mutate(year = year(sale_date)) |> 
   filter(
     year == 2024,
-    sale_city %in% c("shertogenbosch",
-                     "den bosch")   # add variants based on the data 
+    sale_city %in% c(
+      "s hertogenbosch",  
+      "den bosch"
+    )
   ) |> 
   group_by(sale_city) |> 
   summarise(
@@ -19,4 +21,6 @@ avg_prices_denbosch_2024 <- final_output |>
   ) |> 
   arrange(desc(avg_price))
 
+
 avg_prices_denbosch_2024
+
