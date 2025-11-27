d23ac0dd61ce8a7328d5be64db666f94d143830b
miihcr <milena.costaramos@hotmail.com>
2025-11-27 12:25:49 +0100

    Output complete

--- scripts/output.R
+++ scripts/output.R
@@ -5,8 +5,14 @@ final_dataset <- read_rds("data/processed/final_dataset.rds")
 
 final_dataset |>
   filter(address_city == "s hertogenbosch") |>
+  distinct(municipality_id)
+
+final_dataset |>
+  filter(municipality_id == "796") |> # use id here
+  group_by(address_city) |>
   summarise(
-    address_city = first(address_city),
-    avg_price    = mean(price_eur, na.rm = TRUE),
-    n_sales      = n()
+    avg_price = mean(price_eur, na.rm = TRUE),
+    n_sales   = n(),
+    .groups = "drop"
   )
+
