548dfa01c3abd24133ad117ede67db50449396d4
miihcr <milena.costaramos@hotmail.com>
2025-11-27 11:53:51 +0100

    Updates

--- scripts/output.R
+++ scripts/output.R
@@ -1,26 +1,14 @@
 # output (not complete)
 
-final_output <- read_rds("data/processed/final_dataset.rds")
 
-avg_prices_denbosch_2024 <- final_output |> 
-  mutate(year = year(sale_date)) |> 
-  filter(
-    year == 2024,
-    sale_city %in% c(
-      "s hertogenbosch",  
-      "den bosch"
-    )
-  ) |> 
-  group_by(sale_city) |> 
+sales_data  <- read_rds("data/processed/sales_input.rds")
+
+sales_data |> 
+  group_by(town_name) |> 
   summarise(
     n_sales = n(),
-    avg_price = mean(price_eur, na.rm = TRUE),
-    median_price = median(price_eur, na.rm = TRUE),
-    min_price = min(price_eur, na.rm = TRUE),
-    max_price = max(price_eur, na.rm = TRUE)
-  ) |> 
-  arrange(desc(avg_price))
+    avg_price = mean(sales_price_eur, na.rm = TRUE)
+  )
 
-
-avg_prices_denbosch_2024
+  
 
